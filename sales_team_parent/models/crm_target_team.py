910eff7e409676c13fa2bb6b253dd34c78cf744e
William Olhasque <william.olhasque@scopea.fr>
2018-08-22 10:55:22 +0200

    Small update and format

--- sales_team_parent/models/crm_target_team.py
+++ sales_team_parent/models/crm_target_team.py
@@ -25,14 +25,19 @@ from odoo import models, fields, api
 
 
 class CrmTeam(models.Model):
-    _inherit = 'crm.team'
+    _inherit = "crm.team"
 
     def get_currency(self):
         return self.env.user.company_id.currency_id.id
 
-    team_target = fields.Float(string='Team Target', compute="compute_team_target")
-    parent_team = fields.Many2one('crm.team', string='Parent Team', copy=False)
-    currency_id = fields.Many2one('res.currency', string='Currency', readonly=True, default=lambda self: self.get_currency())
+    team_target = fields.Float(string="Team Target", compute="compute_team_target")
+    parent_team = fields.Many2one("crm.team", string="Parent Team", copy=False)
+    currency_id = fields.Many2one(
+        "res.currency",
+        string="Currency",
+        readonly=True,
+        default=lambda self: self.get_currency(),
+    )
 
     @api.one
     def compute_team_target(self):
@@ -41,7 +46,7 @@ class CrmTeam(models.Model):
         childs += self
         for team in childs:
             for member_id in team.member_ids:
-                target = team.env['crm.target'].search([('user_id', '=', member_id.id)])
+                target = team.env["crm.target"].search([("user_id", "=", member_id.id)])
                 if target:
                     for t in target:
                         total += t.target_amount
@@ -50,10 +55,8 @@ class CrmTeam(models.Model):
         return
 
     def get_childs(self, team_id):
-        child_teams = self.search([('parent_team.id', '=', team_id)])
+        child_teams = self.search([("parent_team.id", "=", team_id)])
         if child_teams:
             for team in child_teams:
                 child_teams += self.get_childs(team.id)
         return child_teams
-
-
